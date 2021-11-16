35cbd1aa79248b3fe73a0e3671a8eac13342b693
VincenzoPiparo <v.pipar.97@gmai.com>
2021-11-16 15:57:22 +0100

    scaffholding in

---
+++ css/js/script.js
@@ -0,0 +1,18 @@
+// MILESTONE 1
+// Per i multipli di 3 stampi in console “Fizz” al posto del numero
+//  e per i multipli di 5 stampi in console Buzz.
+
+// Per i numeri che sono sia multipli di 3 che di 5 stampi in console FizzBuzz.
+// MILESTONE 2
+
+// Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
+
+// MILESTONE 3
+// Applica uno stile differente a seconda del valore dell'indice per i multipli di 3,
+// per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
+
+
+// MILESTONE 1
+ for( let i = 1; i <= 1000; i++) {
+     console.log(i)
+ }
